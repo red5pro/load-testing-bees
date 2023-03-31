203f2fe6d7ac5d1d6e3f5c8796a7d4ae4a29af9b
iolesyk <oles@infrared5.com>
2023-03-31 13:03:20 -0400

    Fix WebRTC sub bees script

--- rtcbee/script/rtcbee.sh
+++ rtcbee/script/rtcbee.sh
@@ -3,9 +3,9 @@
 #
 # FILE: rtcbee.sh
 #
-# USAGE: rtcbee.sh [endpoint] [app] [streamName] [amount of streams to start] [amount of time to playback] [old scenario true/false]
+# USAGE: rtcbee.sh [endpoint] [amount of streams to start] [amount of time to playback] [enable old scenario 1 after 1 (true/false)]
 #
-# EXAMPLE: ./rtcbee.sh "your.red5pro-deploy.com" live stream1 10 10 false
+# EXAMPLE: ./rtcbee.sh "https://<RED5PRO-SERVER-HOST>/live/viewer.jsp?host=<RED5PRO-SERVER-HOST>&stream=stream1" 5 30 false
 #
 # DESCRIPTION: Creates N-number of headless WebRTC-based subscriptions to a live stream.
 # Console output sent to log/rtc_sub_streamname_N.log and monitored for status.
@@ -21,32 +21,54 @@
 #===================================================================================
 
 endpoint=$1
-app=$2
-stream_name=$3
-amount=$4
-timeout=$5
-mode=$6 # true (enable old scenario 1 after 1)
+amount=$2
+timeout=$3
+mode=$4 # true (enable old scenario 1 after 1)
 
-log_file="log/rtc_sub"
+if [ "$mode" != true ] ; then
+    mode="false"
+fi
+
+work_dir=$(pwd)
+dir="$work_dir/log/rtc_sub"
+amount_of_directories=$(find ${dir}_* -maxdepth 1 -type d | wc -l)
+current_dir_number=$((amount_of_directories+1))
+current_dir="${dir}_${current_dir_number}"
+mkdir -p "${current_dir}"
+log_file="${current_dir}/rtc_sub"
+
 DEBUG_PORT_START=$(((RANDOM % 10000)+10000));
 
-mkdir -p log
 PIDS=()
 log_i() {
     log
     printf "\033[0;36m [INFO]  --- %s \033[0m\n" "${@}"
+    log >> ${log_file}_main.log
+    echo " [INFO]  --- ${*}" >> ${log_file}_main.log
 }
 log_s() {
     log
     printf "\033[0;32m [START] --- %s \033[0m\n" "${@}"
+    log >> ${log_file}_main.log
+    echo " [START] --- ${*}" >> ${log_file}_main.log
 }
 log_f() {
     log
     printf "\033[0;33m [STOP]  --- %s \033[0m\n" "${@}"
+    log >> ${log_file}_main.log
+    echo " [STOP]  --- ${*}" >> ${log_file}_main.log
 }
 log_w() {
     log
-    printf "\033[0;31m [WARN]  --- %s \033[0m\n" "${@}"
+    printf "\033[0;34m [WARN]  --- %s \033[0m\n" "${@}"
+    log >> ${log_file}_main.log
+    echo " [WARN]  --- ${*}" >> ${log_file}_main.log
+}
+log_e() {
+    log
+    printf "\033[0;31m [ERROR] --- %s \033[0m\n" "${@}"
+    log >> ${log_file}_main.log
+    echo " [ERROR]  --- ${*}" >> ${log_file}_main.log
 }
 log() {
     echo -n "[$(date '+%Y-%m-%d %H:%M:%S')]"
@@ -60,17 +82,17 @@ log() {
 function shutdown {
     local pid=$1
     local beeN=$2
- 
+
     for ((p=1;p<=5;p++)); do
         if ps -p "$pid" > /dev/null
         then
             kill -9 "$pid" > /dev/null 2>&1
         else
-            log_f "Bee #${beeN} --- stopped, PID(${pid})"
+            log_f "Bee #${beeN} --- stopped, PID(${pid}), Log file ${log_file}_${beeN}.log"
             break
         fi
         if [ "$p" -eq "5" ]; then
-            log_w "Bee #${beeN} --- Can't stop, PID(${pid}). Please kill this process manualy in terminal: kill ${pid}"
+            log_e "Bee #${beeN} --- Can't stop, PID(${pid}). Please kill this process manualy in terminal: kill ${pid}"
         fi
         sleep 0.4
     done
@@ -98,98 +120,103 @@ function interrupt {
 
 function checkStatus {
     local pid=$1
-    local timeout=$2
-    local name=$3
-    local beeN=$4
-    
-    fail_counter=10
-    success=0
+    local beeN=$2
+    local tabs=$3
+
+    fail_counter=15
     regex_fail="Subscribe\.Start"
-    
+
     for ((t=1;t<=fail_counter;t++)); do
-        
+        success=0
+
         while read -r line
         do
             if [[ $line =~ $regex_fail ]]; then
-                success=1
+                success=$((success+1))
             fi
-        done < "${log_file}_${name}.log"
-        
-        if [ $success -eq 1 ]; then
-            log_s "Bee #$beeN --- Deployed. Will kill in ${timeout} seconds, PID:${pid}"
+        done < "${log_file}_${beeN}.log"
+
+        if [ $success -eq $tabs ]; then
+            log_s "Bee #$beeN --- Deployed. $success out of ${tabs} Connections(tabs). Will kill in ${timeout} seconds, PID:${pid}"
             sleep "$timeout"
             shutdown "$pid" "${beeN}"
             break
         else
             if [ $t -eq $fail_counter ]; then
-                log_w "Bee #$beeN --- Not deployed. Please check log file ${log_file}_${name}.log and target Red5 pro server!!!"
-                sleep $((timeout-fail_counter))
-                shutdown "$pid" "${beeN}"
+                if [ $success -gt 0 ]; then
+                    log_w "Bee #$beeN --- Partially Deployed. $success out of ${tabs} Connections(tabs). Will kill in ${timeout} seconds. Please check log file ${log_file}_${beeN}.log and target Red5 pro server!!!"
+                    sleep $((timeout-fail_counter))
+                    shutdown "$pid" "${beeN}"
+                else
+                    log_e "Bee #$beeN --- Not deployed. $success out of ${tabs} Connections(tabs). Please check log file ${log_file}_${beeN}.log and target Red5 pro server!!!"
+                    shutdown "$pid" "${beeN}"
+                fi
             fi
             sleep 1
         fi
     done
 }
 
-
 # Dispatch.
 function start_bees {
-    name="${stream_name}_${i}"
-    rm -rf "${log_file}_${name}.log"
+    tabs=$1
     debug_port=$((DEBUG_PORT_START + i))
-    chromium-browser --autoplay-policy=no-user-gesture-required --user-data-dir=/tmp/chrome"$(date +%s%N)" --headless --disable-gpu --mute-audio --remote-debugging-port=$debug_port $endpoint_str 3>&1 1>"${log_file}_${stream_name}_${i}.log" 2>&1 &
+    log_file_current="${log_file}_${i}.log"
+    rm -rf ${log_file_current}
+    log_s "Bee #$i --- Open debug port: $debug_port"
+    #--enable-logging=stderr --v=1
+    chromium-browser --autoplay-policy=no-user-gesture-required --user-data-dir=/tmp/chrome"$(date +%s%N)" --headless --disable-gpu --mute-audio --window-size=1024,768 --remote-debugging-port=$debug_port $endpoint_str 3>&1 1>"${log_file_current}" 2>&1 &
     pid=$!
     PIDS+=("${pid}")
-    sleep 1
+    sleep 0.1
     if [ "$i" -eq "$amount" ]; then
-        (checkStatus "$pid" "$timeout" "$name" "$i")
+        (checkStatus "$pid" "$i" "$tabs")
     else
-        (checkStatus "$pid" "$timeout" "$name" "$i")&
+        (checkStatus "$pid" "$i" "$tabs")&
     fi
-    sleep 1
 }
 
-
-target="https://${endpoint}/${app}/viewer.jsp?host=${endpoint}&stream=${stream_name}"
-
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "--------------------------------------------------" >> ${log_file}_main.log
 log_i "RTC Subscribe bees"
-log_i "Red5 Pro target server: $endpoint"
-log_i "Red5 Pro target URL: $url_endpoint"
-log_i "Stream name: $stream_name"
+log_i "Red5 Pro target URL: $endpoint"
 log_i "Amount of bees $amount"
 log_i "Time to live bees: $timeout"
 log_i "Mode (Old scenario) : $mode"
+log_i "Logs folder: $current_dir"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+echo "--------------------------------------------------" >> ${log_file}_main.log
 
 trap 'interrupt' SIGINT
 
 if [[ "$mode" == "true" ]]; then
     log_i "Enable old scenario 1 after 1"
-    endpoint_str+="$target"
+    endpoint_str+="$endpoint"
     for ((k=1;k<=amount;k++)); do
-        i=$(($i+1))
-        log_s "Bee #$i --- Deploying +1 RTC connection... Target: ${target}"
-        start_bees
+        i=$((i+1))
+        log_s "Bee #$i --- Deploying +1 RTC connection... Target: ${endpoint}"
+        start_bees "1"
     done
 else
-    while [ $amount -gt 0 ]
+    while [ "$amount" -gt 0 ]
     do
-        i=$(($i+1))
+        i=$((i+1))
         if [[ $amount -gt 40 ]]; then
             for ((t=1;t<=40;t++)); do
-                endpoint_str+="$target "
+                endpoint_str+="$endpoint "
             done
-            log_s "Bee #$i --- Deploying +40 RTC connections... Target: ${target}"
-            start_bees
+            log_s "Bee #$i --- Deploying +40 RTC connections... Target: ${endpoint}"
+            start_bees "40"
             endpoint_str=""
-            amount=$(($amount-40))
+            amount=$((amount-40))
         else
             for ((z=1;z<=amount;z++)); do
-                endpoint_str+="$target "
+                endpoint_str+="$endpoint "
             done
-            log_s "Bee #$i --- Deploying +${amount} RTC connections... Target: ${target}"
-            start_bees
+            log_s "Bee #$i --- Deploying +${amount} RTC connections... Target: ${endpoint}"
+            tabs=$amount
+            amount=$i
+            start_bees "$tabs"
             endpoint_str=""
             amount=0
         fi
